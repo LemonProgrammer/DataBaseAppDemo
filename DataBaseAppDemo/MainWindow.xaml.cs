73eb2e450b4069a67b46c15b077b42267e45643a
Fernando Blanco <fern.alej.blanco@gmail.com>
2018-11-16 12:41:47 -0700

    updated stuff

--- DataBaseAppDemo/MainWindow.xaml.cs
+++ DataBaseAppDemo/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,88 @@ namespace DataBaseAppDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        FernandoDBEntities dataEntities = new FernandoDBEntities();
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DbSet<Degree> degrees = dataEntities.Degrees;
+
+            //var query = from degree in degrees
+            //            where degree.Salary > 70000
+            //            orderby degree.Name
+            //            select degree;
+            var query = from degree in degrees
+                        where degree.IsCool == true
+                        orderby degree.Heads
+                        select degree;
+
+            //DegreeDataBase.ItemsSource = degrees.ToList();
+            DegreeDataBase.ItemsSource = query.ToList();
+        }
+
+        private void DegreeDataBase_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            try
+            {
+                FrameworkElement element1 = DegreeDataBase.Columns[0].GetCellContent(e.Row);
+                if (element1.GetType() == typeof(TextBox))
+                {
+                    newDegree.Name = ((TextBox)element1).Text;
+                }
+                FrameworkElement element2 = DegreeDataBase.Columns[1].GetCellContent(e.Row);
+                if (element2.GetType() == typeof(TextBox))
+                {
+                    newDegree.Heads = ((TextBox)element2).Text;
+                }
+                FrameworkElement element3 = DegreeDataBase.Columns[2].GetCellContent(e.Row);
+                if (element3.GetType() == typeof(TextBox))
+                {
+                    newDegree.StudentCount = Convert.ToInt32(((TextBox)element3).Text);
+                }
+                FrameworkElement element4 = DegreeDataBase.Columns[3].GetCellContent(e.Row);
+                if (element4.GetType() == typeof(TextBox))
+                {
+                    newDegree.Salary = Convert.ToDecimal(((TextBox)element4).Text);
+                }
+                FrameworkElement element5 = DegreeDataBase.Columns[4].GetCellContent(e.Row);
+                if (element5.GetType() == typeof(TextBox))
+                {
+                    newDegree.IsCool = Convert.ToBoolean(((TextBox)element5).Text);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        private void DegreeDataBase_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            newDegree = DegreeDataBase.SelectedItem as Degree;
+        }
+
+        private void DegreeDataBase_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            bool exists = false;
+            foreach (Degree degree in dataEntities.Degrees)
+            {
+                if (newDegree == degree || newDegree.Name == degree.Name)
+                {
+                    exists = true;
+                    break;
+
+                }
+
+
+            }
         }
     }
 }
